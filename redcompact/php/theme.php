8c5b712e3f19038abd9d256c42653a0548fc90e9
M. Emin Aksehirli <emin.aksehirli@gmail.com>
2015-03-19 15:13:31 +0100

    First version

---
+++ redcompact/php/theme.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *   * Name: Red Compact
+ *   * Description: A derivative on redbasic with more compact items in coversations.
+ *   * Version: 1.0
+ *   * Author: Emin
+ *   * Compat: Red [*]
+ *
+ */
+
+function redcompact_init(&$a) {
+
+    $a->theme_info['extends'] = 'redbasic';
+
+
+}
